1e7c0f07002120553c18742b936abbc353abf666
Manuel Guengui <manuelguenguii@gmail.com>
2025-08-31 02:41:03 +0100

    remover js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(()=>{
-console.log(window.innerWidth)
-
-if(window.innerWidth > 500){
-  alert("Este site só esta disponiel para versão Mobile" +window.innerWidth);
-  window.location.href = ""
-}
-})();
